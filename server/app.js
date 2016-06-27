bbefe918dbcb02e2da2b6af1902a30f5464d37df
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-06-27 10:41:36 -0500

    Working base MEAN stack

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 // spin up server
 app.listen(3000, 'localhost', function (req, res) {
   console.log('Now serving 3000');
 });
+
+// base URL; console logs to Atom terminal since coming from server side
+app.get('/', function (req, res) {
+  console.log('in base URL');
+  res.sendFile(path.resolve('views/index.html')); // gets this path and sends to base URL as response
+});
+
+// make public folder static
+app.use(express.static('public'));
