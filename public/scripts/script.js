3b8051915daa727f59d281f496ea6c03f0401bb2
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-06-27 11:11:04 -0500

    AngularJS setup and working

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,12 @@
 console.log('script.js sourced in');
+
+// create angular app
+var testApp = angular.module('testApp', []);
+
+// create controller
+testApp.controller('TestController', ['$scope', '$http', function ($scope, $http) { // use $scope which is built into Angular. scope of controller. function uses $scope and calls it $scope. function also uses http
+  // test getting user input
+  $scope.whatAreWeTryingToDo = function () {
+    console.log('in whatAreWeTryingToDo function: ' + $scope.testBinder);
+  };
+}]);
